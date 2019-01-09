218220a2ce044d04c3057144e1e0008c2c50ccb2
BT_1N3_27 <wegr>
2019-01-09 14:45:28 -0500

    i like turtles

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -53,51 +53,26 @@ public class InPlaceSorts
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        public static void swap(int[]arr,int i, int j){
-            int a = arr[j];
-            int b = arr[i];
-            arr[j] = b;
-            arr[i] = a;
+    public static void swap(String[]arr,int i, int j){
+        String a = arr[j];
+        String b = arr[i];
+        arr[j] = b;
+        arr[i] = a;
         }
 
-        public static void bubleswap(int [ ] arr){
-            boolean confirm = false;
-            while(confirm != true){
-                confirm = true;
-                for(int i=0; i <arr.length-1;i++){
-                    if(arr[i] < arr[i+1]){
-                    }
-                    if(arr[i] > arr[i+1]){
-                        swap(arr,i,i+1);
-                        confirm = false;
-                    }
+    public static void bubleswap(String [ ] arr){
+        boolean confirm = false;
+        while(confirm != true){
+            confirm = true;
+            for(int i=0; i <arr.length-1;i++){
+                if(arr[i].compareTo(arr[i+1]) > 0){
+                    swap(arr,i,i+1);
+                    confirm = false;
                 }
             }
         }
-        public static boolean checkSum(int [] before,int []after){
+    }
+    public static boolean checkSum(int [] before,int []after){
             int w = 0;
             for(int i =0 ; i < before.length;i++){
                 w += before[i];
@@ -111,7 +86,7 @@ public class InPlaceSorts
             }
             return true;
         }
-        public static boolean sortCheck(int [] arr){
+    public static boolean sortCheck(int [] arr){
             boolean inOrder = true;
             for (int i = 0; i < arr.length; i ++){
                 if( arr[i] > arr[i+1]){
@@ -120,5 +95,22 @@ public class InPlaceSorts
             }
             return inOrder;
         }
+    public String[] randomStringArr(int num, int length){
+        String [] arr = new String [num];
+        while (num > 0)
+        {
+            int i = 0;
+            String s = "";
+            while (i < length)
+            {
+             char c = (char)((Math.random()*26)+97);
+             s = s + c;
+             i++;
+            }
+            num--;
+            arr[num] = s;
+        }
+        return arr;
     }
+}
 

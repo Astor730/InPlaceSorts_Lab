c490d4bdf3c2a6939af57ef048d216751f0cfb59
BT_1N3_26 <a>
2019-01-11 14:28:19 -0500

    Organization

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -19,36 +19,17 @@ public class InPlaceSorts
             }
         }
     }
-    public static void doubleSwap(double[] arr, int i,int j)
-    {
-        double a = arr[i];
-        arr[i] = arr[j];
-        arr[j] = a;
-    }
-    public static void intSwap(int[] arr, int i, int j)
-    {
-        int a = arr[i];
-        arr[i] = arr[j];
-        arr[j] = a;
-
-    }
-    public static double[] randDouble(int count)
-    {
-        double[] big = new double[count];
-        for(int i = 0; i<big.length;i++)
-        {
-            big[i] = Math.random()*10001;
+    public static void bubleSort(String [ ] arr){
+        boolean confirm = false;
+        while(confirm != true){
+            confirm = true;
+            for(int i=0; i <arr.length-1;i++){
+                if(arr[i].compareTo(arr[i+1]) > 0){
+                    stringSwap(arr,i,i+1);
+                    confirm = false;
+                }
+            }
         }
-        return big;
-    }
-    public static int[] randint(int count)
-    {
-        int[] big = new int[count];
-        for(int i = 0; i<big.length; i++)
-        {
-            big[i] = (int) (Math.random()*10001);
-        }
-        return big;
     }
     public static void insertionSort(int[] arr)
     {
@@ -64,42 +45,58 @@ public class InPlaceSorts
             }
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public static void swap(String[]arr,int i, int j){
+    public static void doubleSwap(double[] arr, int i,int j)
+    {
+        double a = arr[i];
+        arr[i] = arr[j];
+        arr[j] = a;
+    }
+    public static void intSwap(int[] arr, int i, int j)
+    {
+        int a = arr[i];
+        arr[i] = arr[j];
+        arr[j] = a;
+    }
+    public static void stringSwap(String[]arr,int i, int j){
         String a = arr[j];
         String b = arr[i];
         arr[j] = b;
         arr[i] = a;
+    }
+    public static double[] randDouble(int count)
+    {
+        double[] big = new double[count];
+        for(int i = 0; i<big.length;i++)
+        {
+            big[i] = Math.random()*10001;
         }
-
-    public static void bubleswap(String [ ] arr){
-        boolean confirm = false;
-        while(confirm != true){
-            confirm = true;
-            for(int i=0; i <arr.length-1;i++){
-                if(arr[i].compareTo(arr[i+1]) > 0){
-                    swap(arr,i,i+1);
-                    confirm = false;
-                }
+        return big;
+    }
+    public static int[] randInt(int count)
+    {
+        int[] big = new int[count];
+        for(int i = 0; i<big.length; i++)
+        {
+            big[i] = (int) (Math.random()*10001);
+        }
+        return big;
+    }
+    public String[] randomStringArr(int num, int length){
+        String [] arr = new String [num];
+        while (num > 0)
+        {
+            int i = 0;
+            String s = "";
+            while (i < length)
+            {
+                char c = (char)((Math.random()*26)+97);
+                s = s + c;
+                i++;
             }
+            num--;
+            arr[num] = s;
         }
+        return arr;
     }
     public static boolean checkSum(int [] before,int []after){
             int w = 0;
@@ -124,22 +121,6 @@ public class InPlaceSorts
             }
             return inOrder;
         }
-    public String[] randomStringArr(int num, int length){
-        String [] arr = new String [num];
-        while (num > 0)
-        {
-            int i = 0;
-            String s = "";
-            while (i < length)
-            {
-             char c = (char)((Math.random()*26)+97);
-             s = s + c;
-             i++;
-            }
-            num--;
-            arr[num] = s;
-        }
-        return arr;
-    }
+
 }
 
